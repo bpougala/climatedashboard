202808a3caa8a36f711af0d981d3b18426c6c6a1
Biko Pougala <bpougala@gmail.com>
2022-01-07 20:48:23 +0100

    Working on integrating D3.js

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { FirebaseStorage, getStorage, ref, getDownloadURL } from 'firebase/storage';
-import { csvParseRows, select, max, min } from 'd3';
+import * as d3 from 'd3';
 
 const firebaseConfig = {
     apiKey: "AIzaSyDmIGZPEvy6nxDQyRw3o_JM2d9288p_fFU",
@@ -15,6 +15,9 @@ const storage : FirebaseStorage = getStorage(app);
 const margin = {top: 10, right: 30, bottom: 30, left: 60},
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
+let xNudge = 50;
+let yNudge = 20;
+
 getDownloadURL(ref(storage, 'co2_annmean_mlo.csv'))
 .then((url) => {
     const xhr = new XMLHttpRequest();
@@ -23,21 +26,34 @@ getDownloadURL(ref(storage, 'co2_annmean_mlo.csv'))
         if(xhr.readyState === 4) {
             const blob : string = xhr.response;
             const arr = CSVtoArray(blob);
-            var svg = select("#co2_visualisation")
+            var svg = d3.select("#co2_visualisation")
                 .append("svg")
                 .attr("width", width + margin.left + margin.right)
                 .attr("height", height + margin.top + margin.bottom)
                 .append("g")
                 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-            const multiDimArray = csvParseRows(arr)
-            const maxYear : number = max(multiDimArray.slice(1), function(line) { return parseInt(line[0])});
-            const minYear : number = min(multiDimArray.slice(1), function(line) { return parseInt(line[0])});
-            const maxCarbon : number = max(multiDimArray.slice(1), function(line) { return parseInt(line[1])});
-            const minCarbon : number = min(multiDimArray.slice(1), function(line) { return parseInt(line[1])});
-            console.log("maxYear: " + maxYear);
-            console.log("minYear: " + minYear);
-            console.log("maxCarbon: " + maxCarbon);
-            console.log("minCarbon: " + minCarbon);
+            const multiDimArray = d3.csvParseRows(arr)
+            const maxYear : number = d3.max(multiDimArray.slice(1), function(line) { return parseInt(line[0])});
+            const minYear : number = d3.min(multiDimArray.slice(1), function(line) { return parseInt(line[0])});
+            const maxCarbon : number = d3.max(multiDimArray.slice(1), function(line) { return parseInt(line[1])});
+            const minCarbon : number = d3.min(multiDimArray.slice(1), function(line) { return parseInt(line[1])});
+            const y = d3.scaleLinear().domain([minCarbon, maxCarbon])
+                .range([height, 0]);
+
+            const x = d3.scaleLinear().domain([minYear, maxYear])
+                .range([0, width]);
+
+            const yAxis = d3.axisLeft(y);
+            const xAxis = d3.axisRight(x);
+            const line = d3.line()
+                .x(function(elem) { return elem[0]; })
+                .y(function(elem) { return elem[1]; })
+                .curve(d3.curveCardinal);
+
+            const rows = multiDimArray.slice(1).map(flattenArray);
+            svg.append('g')
+                .selectAll('dot')
+                .data(rows)
         }
     };
     xhr.open('GET', url);
@@ -47,6 +63,10 @@ getDownloadURL(ref(storage, 'co2_annmean_mlo.csv'))
     console.error(error);
 })
 
+function flattenArray(subArray : Array<string>) {
+    const trimArray = subArray.slice(0, 1);
+    return trimArray.map(elem => parseInt(elem));
+}
 function CSVtoArray(rawText : string) {
     const regex = /^[^#].*/;
     const temp : Array<string> = rawText.split("\n");
